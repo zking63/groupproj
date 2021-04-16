affdf191546fcd8cc5dcc743715b5290d4f223ac
zking63 <zoemking63@gmail.com>
2021-04-16 13:28:07 -0400

    home controller updated

--- src/main/java/com/codingdojo/GroupProject/Controllers/HomeController.java
+++ src/main/java/com/codingdojo/GroupProject/Controllers/HomeController.java
@@ -1,5 +1,15 @@
 package com.codingdojo.GroupProject.Controllers;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.codingdojo.GroupProject.models.User;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String index() {
+		return "home.jsp";
+	}
 }
